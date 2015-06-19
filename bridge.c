a7a9c14df4cf9940dc48cfdb398892561699c9ab
Xenomorff <brandonjenks32400@gmail.com>
2015-06-18 20:46:08 -0400

    save

--- bridge.c
+++ bridge.c
@@ -4,20 +4,60 @@
 #include "readline.h"
 
 #define _GNU_SOURCE
-#define END "GR8 B8 M8\n"
-#define MIDDLE "Y M8\n"
-#define INTRO "U WOT M8\n"
+#define INTRO "WHO IS U\n"
+
+char* ask(char* question) {
+  char * answer;
+  printf(RED"%s\n"RESET,question);
+  answer = input(">> ");
+  printf(RESET);
+  return answer;
+}
+
+
+live()
+{
+  printf("Live\n");
+}
+
+die()
+{
+  printf("Die\n");
+}
+
 
 main()
 {
-  printf(RED INTRO);
-  char* intro = input(YELLOW">>"BASE3);
-  
-  printf(ORANGE MIDDLE);
-  char* mid = input(YELLOW">>"BASE3);
+  printf(BASE3 INTRO"\n");
+ 
+  char * name = ask("What is your name?");
+  char * quest = ask("What is your quest?");
+  if (strcmp(name,"lance\n") == 0 || strcmp(name,"galahad\n ") == 0)
+    {
+      char * answer = ask("What is fav color?");
+      if (strcmp(answer,"blue\n") == 0)
+        {
+          live();
+        }
+      else
+        {
+          die();
+        }
+    }
 
-  printf(PINK END);
-  char* end = input(YELLOW">>"BASE3);
 
-  //if (strcmp(name
+  else if (strcmp(name,"robin\n") == 0) {
+    char * answer = ask("cap");
+    if(strcmp(answer,"7\n") == 0 || strcmp(answer,"8\n") == 0)
+      {
+        live();
+      }
+    else
+      {
+        die();
+      }
+
+
+
+    }
 }
